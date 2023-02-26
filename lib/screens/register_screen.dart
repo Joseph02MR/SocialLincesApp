eab0da3ec5cb2a26d09eb2a0aab620cdeca8bdb1
José Montero <67172507+Joseph02MR@users.noreply.github.com>
2023-02-25 23:13:27 -0600

    practica 1 finalizada

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -33,16 +33,34 @@ class _RegisterScreenState extends State<RegisterScreen> {
     });
   }
 
+  String? validateEmail(String? value) {
+    const pattern = r"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'"
+        r'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-'
+        r'\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*'
+        r'[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4]'
+        r'[0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9]'
+        r'[0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\'
+        r'x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])';
+    final regex = RegExp(pattern);
+
+    return value!.isNotEmpty && !regex.hasMatch(value)
+        ? 'Enter a valid email address'
+        : null;
+  }
+
   void validate_textFields() {
     var textAux = 'Error: hay campos vacíos';
     if (cmfPassword.text != password.text) {
-      textAux = 'Las contraseñas no coinciden';
-    }
-    if (email.text != "" &&
+      textAux = 'Error: Las contraseñas no coinciden';
+    } else if (email.text != "" &&
         password.text != "" &&
         cmfPassword.text != "" &&
         name.text != "") {
       textAux = '¡Registro exitoso!';
+
+      if (validateEmail(email.text) != null) {
+        textAux = 'Error: correo no válido';
+      }
     }
 
     showDialog(
