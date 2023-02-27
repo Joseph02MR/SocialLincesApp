254ead6de618afdf9c0f13baad9d303c83477cd0
José Montero <67172507+Joseph02MR@users.noreply.github.com>
2023-02-27 12:08:58 -0600

    Cambios Práctica 2

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/screens/responsive.dart';
 import 'package:flutter_application_1/widgets/loading_modal_widget.dart';
 import 'package:social_login_buttons/social_login_buttons.dart';
 
@@ -63,50 +64,172 @@ class _LoginScreenState extends State<LoginScreen> {
 
     final imgLogo = Image.asset(
       'assets/logo_itc.png',
-      height: 250,
     );
 
     return Scaffold(
       resizeToAvoidBottomInset: true,
-      body: Stack(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-                image: DecorationImage(
-                    opacity: 0.5,
-                    fit: BoxFit.cover,
-                    image: AssetImage('assets/itc_esc.jpg'))),
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 10),
-              child: Stack(
-                alignment: Alignment.topCenter,
+      body: Responsive(
+        mobile: MobileLoginScreen(
+            txtEmail: txtEmail,
+            horizontalSpace: horizontalSpace,
+            txtPass: txtPass,
+            buttonlogging: buttonlogging,
+            googlebtn: googlebtn,
+            btnGithub: btnGithub,
+            txtRegister: txtRegister,
+            imgLogo: imgLogo,
+            isLoading: isLoading),
+        desktop: DesktopLoginScreen(
+          imgLogo: imgLogo,
+          txtEmail: txtEmail,
+          horizontalSpace: horizontalSpace,
+          txtPass: txtPass,
+          buttonlogging: buttonlogging,
+          googlebtn: googlebtn,
+          btnGithub: btnGithub,
+          txtRegister: txtRegister,
+          isLoading: isLoading,
+        ),
+      ),
+    );
+  }
+}
+
+class DesktopLoginScreen extends StatelessWidget {
+  const DesktopLoginScreen({
+    Key? key,
+    required this.imgLogo,
+    required this.txtEmail,
+    required this.horizontalSpace,
+    required this.txtPass,
+    required this.buttonlogging,
+    required this.googlebtn,
+    required this.btnGithub,
+    required this.txtRegister,
+    required this.isLoading,
+  }) : super(key: key);
+
+  final Image imgLogo;
+  final TextFormField txtEmail;
+  final SizedBox horizontalSpace;
+  final TextFormField txtPass;
+  final SocialLoginButton buttonlogging;
+  final SocialLoginButton googlebtn;
+  final SocialLoginButton btnGithub;
+  final Padding txtRegister;
+  final bool isLoading;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  opacity: 0.5,
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/itc_esc.jpg'))),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: imgLogo,
+            ),
+            SizedBox(
+              width: 450,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Column(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      txtEmail,
-                      horizontalSpace,
-                      txtPass,
-                      horizontalSpace,
-                      buttonlogging,
-                      horizontalSpace,
-                      googlebtn,
-                      horizontalSpace,
-                      btnGithub,
-                      txtRegister
-                    ],
-                  ),
-                  Positioned(
-                    top: 100,
-                    child: imgLogo,
-                  )
+                  txtEmail,
+                  horizontalSpace,
+                  txtPass,
+                  horizontalSpace,
+                  buttonlogging,
+                  horizontalSpace,
+                  googlebtn,
+                  horizontalSpace,
+                  btnGithub,
+                  txtRegister
                 ],
               ),
             ),
+            const Padding(padding: EdgeInsets.only(right: 100))
+          ],
+        ),
+        isLoading ? const LoadingModalWidget() : Container()
+      ],
+    );
+  }
+}
+
+class MobileLoginScreen extends StatelessWidget {
+  const MobileLoginScreen({
+    Key? key,
+    required this.txtEmail,
+    required this.horizontalSpace,
+    required this.txtPass,
+    required this.buttonlogging,
+    required this.googlebtn,
+    required this.btnGithub,
+    required this.txtRegister,
+    required this.imgLogo,
+    required this.isLoading,
+  }) : super(key: key);
+
+  final TextFormField txtEmail;
+  final SizedBox horizontalSpace;
+  final TextFormField txtPass;
+  final SocialLoginButton buttonlogging;
+  final SocialLoginButton googlebtn;
+  final SocialLoginButton btnGithub;
+  final Padding txtRegister;
+  final Image imgLogo;
+  final bool isLoading;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+                  opacity: 0.5,
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/itc_esc.jpg'))),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    txtEmail,
+                    horizontalSpace,
+                    txtPass,
+                    horizontalSpace,
+                    buttonlogging,
+                    horizontalSpace,
+                    googlebtn,
+                    horizontalSpace,
+                    btnGithub,
+                    txtRegister
+                  ],
+                ),
+                SizedBox(
+                  height: 250,
+                  child: Positioned(
+                    top: 100,
+                    child: imgLogo,
+                  ),
+                )
+              ],
+            ),
           ),
-          isLoading ? const LoadingModalWidget() : Container()
-        ],
-      ),
+        ),
+        isLoading ? const LoadingModalWidget() : Container()
+      ],
     );
   }
 }
