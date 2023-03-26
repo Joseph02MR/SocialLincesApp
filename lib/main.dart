7fb39415d7d753a43968da1b2f05cb5a96d7b609
José Montero <67172507+Joseph02MR@users.noreply.github.com>
2023-03-25 19:35:04 -0600

    firebase integration 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/provider/flags_provider.dart';
 import 'package:flutter_application_1/provider/theme_provider.dart';
@@ -5,7 +6,11 @@ import 'package:flutter_application_1/routes.dart';
 import 'package:flutter_application_1/screens/login_screen.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
